91c4c570acd9e789b19f2d7b5ea35f211a81ddce
Wellington Ayres <145718802+Wlayres@users.noreply.github.com>
2024-05-23 16:02:08 -0300

    Conexao com o banco de dados

---
+++ BACKEND/conexaoDB.php
@@ -0,0 +1,12 @@
+<?php 
+$Shost = 'swanshine.cpkoaos0ad68.us-east-2.rds.amazonaws.com';
+$usuario = 'admin';
+$senha = 'gLAHqWkvUoaxwBnm9wKD';  
+$banco_de_dados = 'swanshine'; 
+
+$conn = mysqli_connect($Shost, $usuario, $senha, $banco_de_dados); 
+
+if (!$conn) {
+    die('Erro ao conectar ao banco de dados: ' . mysqli_connect_error()); 
+}
+?>
