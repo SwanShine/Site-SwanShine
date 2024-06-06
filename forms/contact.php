d8893922506ead49c62f91e4a35515239ad61fbe
Thiago Nicolas <profissional.thiagonicolas@gmail.com>
2024-06-06 10:45:13 -0300

    .

--- forms/contact.php
+++ forms/contact.php
@@ -1,18 +1,18 @@
 <?php
   /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
+  * Requer a biblioteca "PHP Email Form"
+  * A biblioteca "PHP Email Form" está disponível apenas na versão pro do modelo
+  * A biblioteca deve ser enviada para: vendor/php-email-form/php-email-form.php
+  * Para mais informações e ajuda: https://bootstrapmade.com/php-email-form/
   */
 
-  // Replace contact@example.com with your real receiving email address
+  // Substitua contact@example.com pelo seu endereço de email real para recebimento
   $receiving_email_address = 'contact@example.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die( 'Não foi possível carregar a biblioteca "PHP Email Form"!');
   }
 
   $contact = new PHP_Email_Form;
@@ -23,7 +23,7 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  // Descomente o código abaixo se você quiser usar SMTP para enviar emails. Você precisa inserir suas credenciais SMTP corretas
   /*
   $contact->smtp = array(
     'host' => 'example.com',
@@ -33,9 +33,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['name'], 'De');
   $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( $_POST['message'], 'Mensagem', 10);
 
   echo $contact->send();
 ?>
